dad5dd107cc307cd8f2a7979195077b48267f1aa
joaochecchia <142359136+joaochecchia@users.noreply.github.com>
2024-11-21 21:54:43 -0300

    imagem na tela de loggin

--- BancoMalvader/src/main/java/view/LoginScreen.java
+++ BancoMalvader/src/main/java/view/LoginScreen.java
@@ -1,6 +1,7 @@
 package view;
-import javax.swing.*;
 
+import javax.swing.*;
+import java.awt.*;
 import dao.ClienteDAO;
 import dao.FuncionarioDAO;
 import dao.LoginDAO;
@@ -10,7 +11,7 @@ import model.Funcionario;
 public class LoginScreen extends JFrame {
     public LoginScreen() {
         setTitle("Banco Malvader - Login");
-        setSize(300, 150);
+        setSize(300, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
@@ -18,28 +19,38 @@ public class LoginScreen extends JFrame {
         add(panel);
         panel.setLayout(null);
 
+        // Adiciona a imagem redimensionada acima dos campos
+        ImageIcon originalIcon = new ImageIcon("C:\\Users\\jjgab\\OneDrive\\Documentos\\GitHub\\banco-Malvader\\BancoMalvader\\src\\main\\resources\\ImageIcon.png"); // Substitua pelo caminho correto
+        Image originalImage = originalIcon.getImage();
+        Image resizedImage = originalImage.getScaledInstance(64, 64, Image.SCALE_SMOOTH); // Define a largura e altura
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+
+        JLabel imageLabel = new JLabel(resizedIcon);
+        imageLabel.setBounds(110, 10, 64, 64); // Ajusta a posição da imagem
+        panel.add(imageLabel);
+
         JLabel userLabel = new JLabel("Usuário:");
-        userLabel.setBounds(10, 20, 80, 25);
+        userLabel.setBounds(10, 90, 80, 25);
         panel.add(userLabel);
 
         JTextField userText = new JTextField(20);
-        userText.setBounds(100, 20, 165, 25);
+        userText.setBounds(100, 90, 165, 25);
         panel.add(userText);
 
         JLabel passwordLabel = new JLabel("Senha:");
-        passwordLabel.setBounds(10, 50, 80, 25);
+        passwordLabel.setBounds(10, 120, 80, 25);
         panel.add(passwordLabel);
 
         JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100, 50, 165, 25);
+        passwordText.setBounds(100, 120, 165, 25);
         panel.add(passwordText);
 
         JButton loginButton = new JButton("Login");
-        loginButton.setBounds(10, 80, 80, 25);
+        loginButton.setBounds(10, 160, 80, 25);
         panel.add(loginButton);
 
         JCheckBox employeeCheckBox = new JCheckBox("Funcionário");
-        employeeCheckBox.setBounds(100, 80, 100, 25);
+        employeeCheckBox.setBounds(100, 160, 100, 25);
         panel.add(employeeCheckBox);
 
         loginButton.addActionListener(e -> {
@@ -50,19 +61,17 @@ public class LoginScreen extends JFrame {
             LoginDAO login = new LoginDAO();
             boolean verify = login.realizarLogin(usuario, senha, isEmployee);
 
-
             if (verify) {
                 JOptionPane.showMessageDialog(this, "Login bem-sucedido!");
 
-                if(isEmployee){
+                if (isEmployee) {
                     FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
                     Funcionario funcionario = funcionarioDAO.getClassFuncionario(usuario);
 
                     TelaMenuFuncionario telaFuncionario = new TelaMenuFuncionario(funcionario);
                     telaFuncionario.setVisible(true);
                     dispose();
-                } else{
-
+                } else {
                     ClienteDAO clienteDAO = new ClienteDAO();
                     Cliente cliente = clienteDAO.getClasseCliente(usuario);
 
